258526f0161525791a93de887472795dc7d7509c
prygrn <pierremogrison@gmail.com>
2019-09-13 00:10:21 +0200

    Rename lib, rename Node structure and update APIs

---
+++ Core/SinglyLinkedList.h
@@ -0,0 +1,37 @@
+#ifndef SINGLELINKEDLIST_H_
+#define SINGLELINKEDLIST_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "../common.h"
+
+typedef struct SinglyLinkedListNode
+{
+	int data;
+	struct SinglyLinkedListNode *next;
+}SinglyLinkedListNode;
+
+SinglyLinkedListNode* createSinglyLinkedList(int data);
+unsigned int getLinkedListSize(SinglyLinkedListNode* head);
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* head, int data);
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data);
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position);
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position);
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head);
+bool compareLinkedLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2);
+SinglyLinkedListNode* mergeLinkedLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2);
+int getNode(SinglyLinkedListNode* head, int positionFromTail);
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head);
+bool bHasListCycle(SinglyLinkedListNode* head);
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2);
+void printLinkedList(SinglyLinkedListSinglyLinkedListNode* head);
+void reversePrint(SinglyLinkedListNode* head);
+
+/* To be created: 
+- SinglyLinkedListNode*	sortLinkedList(SinglyLinkedListNode* head);
+- bool destroyList(SinglyLinkedListNode* head);
+- SinglyLinkedListNode* updateNode(SinglyLinkedListNode* head, SinglyLinkedListNode* node);
+*/
+
+#endif /* SINGLELINKEDLIST_H_ */
